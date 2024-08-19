cf68c5616944351e4d6e2df306f47d7bc6376a73
FFF <spock@FFFs-iMac.local>
2024-08-20 01:59:43 +0400

    fourth commit

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "test" {
 
   ami           = "ami-04a81a99f5ec58529"
   instance_type = "t2.micro"
-  vpc_id = "vpc-0d20c97da752ee7fb"
+  vpc_security_group_ids = ["vpc-0d20c97da752ee7fb"]
   tags = {
     Name = "Test EC2 Instance"
   }
